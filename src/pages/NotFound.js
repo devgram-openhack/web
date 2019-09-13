82751cc5164ef728fc0bf27af55eda9be2a62e10
rafaelgssa <rafael.gssa@gmail.com>
2019-09-13 14:35:20 -0300

    Create web base

---
+++ src/pages/NotFound.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import './NotFound.css';
+
+/**
+ * Creates the not found element.
+ *
+ * @returns {React.ReactElement} The not found element.
+ */
+function NotFound() {
+  return (
+    <div className="not-found-container">
+      NOT FOUND
+    </div>
+  );
+}
+
+export { NotFound };
